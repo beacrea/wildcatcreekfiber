a16a764896e3703d2fd3c44217c871187643277c
Coty Beasley <coty@cotybeasley.name>
2018-12-23 14:59:21 -0600

    Cleaned up some content

--- src/components/pages/About.vue
+++ src/components/pages/About.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="about">
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur dolor eius ex quam! Consectetur ipsum odio reprehenderit voluptatibus voluptatum. Animi architecto consectetur quaerat soluta. Dicta illum laudantium libero nulla rem!</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur dolor eius ex quam! Consectetur ipsum odio reprehenderit voluptatibus voluptatum. Animi architecto consectetur quaerat soluta. Dicta illum laudantium libero nulla rem! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus accusantium animi consequatur doloremque, dolores explicabo fugiat harum in incidunt iusto magnam mollitia nesciunt nulla quisquam quod recusandae sapiente voluptate voluptatem! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur dolor eius ex quam! Consectetur ipsum odio reprehenderit voluptatibus voluptatum. Animi architecto consectetur quaerat soluta. Dicta illum laudantium libero nulla rem! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus accusantium animi consequatur doloremque, dolores explicabo fugiat harum in incidunt iusto magnam mollitia nesciunt nulla quisquam quod recusandae sapiente voluptate voluptatem!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur dolor eius ex quam! Consectetur ipsum odio reprehenderit voluptatibus voluptatum. Animi architecto consectetur quaerat soluta. Dicta illum laudantium libero nulla rem! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus accusantium animi consequatur doloremque, dolores explicabo fugiat harum in incidunt iusto magnam mollitia nesciunt nulla quisquam quod recusandae sapiente voluptate voluptatem!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur dolor eius ex quam! Consectetur ipsum odio reprehenderit voluptatibus voluptatum. Animi architecto consectetur quaerat soluta. Dicta illum laudantium libero nulla rem! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus accusantium animi consequatur doloremque, dolores explicabo fugiat harum in incidunt iusto magnam mollitia nesciunt nulla quisquam quod recusandae sapiente voluptate voluptatem!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur dolor eius ex quam! Consectetur ipsum odio reprehenderit voluptatibus voluptatum. Animi architecto consectetur quaerat soluta. Dicta illum laudantium libero nulla rem! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus accusantium animi consequatur doloremque, dolores explicabo fugiat harum in incidunt iusto magnam mollitia nesciunt nulla quisquam quod recusandae sapiente voluptate voluptatem!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur dolor eius ex quam! Consectetur ipsum odio reprehenderit voluptatibus voluptatum. Animi architecto consectetur quaerat soluta. Dicta illum laudantium libero nulla rem! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus accusantium animi consequatur doloremque, dolores explicabo fugiat harum in incidunt iusto magnam mollitia nesciunt nulla quisquam quod recusandae sapiente voluptate voluptatem!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur dolor eius ex quam! Consectetur ipsum odio reprehenderit voluptatibus voluptatum. Animi architecto consectetur quaerat soluta. Dicta illum laudantium libero nulla rem! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus accusantium animi consequatur doloremque, dolores explicabo fugiat harum in incidunt iusto magnam mollitia nesciunt nulla quisquam quod recusandae sapiente voluptate voluptatem!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur dolor eius ex quam! Consectetur ipsum odio reprehenderit voluptatibus voluptatum. Animi architecto consectetur quaerat soluta. Dicta illum laudantium libero nulla rem! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus accusantium animi consequatur doloremque, dolores explicabo fugiat harum in incidunt iusto magnam mollitia nesciunt nulla quisquam quod recusandae sapiente voluptate voluptatem!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur dolor eius ex quam! Consectetur ipsum odio reprehenderit voluptatibus voluptatum. Animi architecto consectetur quaerat soluta. Dicta illum laudantium libero nulla rem! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus accusantium animi consequatur doloremque, dolores explicabo fugiat harum in incidunt iusto magnam mollitia nesciunt nulla quisquam quod recusandae sapiente voluptate voluptatem!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur dolor eius ex quam! Consectetur ipsum odio reprehenderit voluptatibus voluptatum. Animi architecto consectetur quaerat soluta. Dicta illum laudantium libero nulla rem! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus accusantium animi consequatur doloremque, dolores explicabo fugiat harum in incidunt iusto magnam mollitia nesciunt nulla quisquam quod recusandae sapiente voluptate voluptatem!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur dolor eius ex quam! Consectetur ipsum odio reprehenderit voluptatibus voluptatum. Animi architecto consectetur quaerat soluta. Dicta illum laudantium libero nulla rem! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus accusantium animi consequatur doloremque, dolores explicabo fugiat harum in incidunt iusto magnam mollitia nesciunt nulla quisquam quod recusandae sapiente voluptate voluptatem!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur dolor eius ex quam! Consectetur ipsum odio reprehenderit voluptatibus voluptatum. Animi architecto consectetur quaerat soluta. Dicta illum laudantium libero nulla rem! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus accusantium animi consequatur doloremque, dolores explicabo fugiat harum in incidunt iusto magnam mollitia nesciunt nulla quisquam quod recusandae sapiente voluptate voluptatem!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur dolor eius ex quam! Consectetur ipsum odio reprehenderit voluptatibus voluptatum. Animi architecto consectetur quaerat soluta. Dicta illum laudantium libero nulla rem! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus accusantium animi consequatur doloremque, dolores explicabo fugiat harum in incidunt iusto magnam mollitia nesciunt nulla quisquam quod recusandae sapiente voluptate voluptatem!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur dolor eius ex quam! Consectetur ipsum odio reprehenderit voluptatibus voluptatum. Animi architecto consectetur quaerat soluta. Dicta illum laudantium libero nulla rem! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus accusantium animi consequatur doloremque, dolores explicabo fugiat harum in incidunt iusto magnam mollitia nesciunt nulla quisquam quod recusandae sapiente voluptate voluptatem!</p>
   </div>
 </template>
 
@@ -25,106 +25,5 @@ export default {
   flex-direction: column;
   overflow-y: scroll;
   height: 100%;
-}
-section {
-  flex: 1 1 auto;
-  display: flex;
-  flex-direction: column;
-  min-height: 100%;
-  align-items: center;
-  header {
-    text-align: left;
-    align-self: flex-start;
-    flex-direction: column;
-    flex: 100%;
-    display: flex;
-    justify-content: center;
-  }
-  &#intro {
-    .downArrow {
-      display: inline-block;
-      width: 5.8rem;
-      height: 4.1rem;
-      animation: bounce ease-in 1s;
-      animation-iteration-count: infinite;
-      transform-origin: 50% 100%;
-      -webkit-animation: bounce ease-in 1s;
-      -webkit-animation-iteration-count: infinite;
-      -webkit-transform-origin: 50% 100%;
-      -moz-animation: bounce ease-in 1s;
-      -moz-animation-iteration-count: infinite;
-      -moz-transform-origin: 50% 100%;
-      -o-animation: bounce ease-in 1s;
-      -o-animation-iteration-count: infinite;
-      -o-transform-origin: 50% 100%;
-      -ms-animation: bounce ease-in 1s;
-      -ms-animation-iteration-count: infinite;
-      -ms-transform-origin: 50% 100%;
-    }
-  }
-}
-header {
-  margin-bottom: 25%;
-  padding-right: 3.2rem;
-}
-
-@keyframes bounce{
-  0% {
-    transform:  translate(0px,0px)  ;
-  }
-  50% {
-    transform:  translate(0px,-16px)  ;
-  }
-  100% {
-    transform:  translate(0px,0px)  ;
-  }
-}
-
-@-moz-keyframes bounce{
-  0% {
-    -moz-transform:  translate(0px,0px)  ;
-  }
-  50% {
-    -moz-transform:  translate(0px,-16px)  ;
-  }
-  100% {
-    -moz-transform:  translate(0px,0px)  ;
-  }
-}
-
-@-webkit-keyframes bounce {
-  0% {
-    -webkit-transform:  translate(0px,0px)  ;
-  }
-  50% {
-    -webkit-transform:  translate(0px,-16px)  ;
-  }
-  100% {
-    -webkit-transform:  translate(0px,0px)  ;
-  }
-}
-
-@-o-keyframes bounce {
-  0% {
-    -o-transform:  translate(0px,0px)  ;
-  }
-  50% {
-    -o-transform:  translate(0px,-16px)  ;
-  }
-  100% {
-    -o-transform:  translate(0px,0px)  ;
-  }
-}
-
-@-ms-keyframes bounce {
-  0% {
-    -ms-transform:  translate(0px,0px)  ;
-  }
-  50% {
-    -ms-transform:  translate(0px,-16px)  ;
-  }
-  100% {
-    -ms-transform:  translate(0px,0px)  ;
-  }
 }
 </style>
