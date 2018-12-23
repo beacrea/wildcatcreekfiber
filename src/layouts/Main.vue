c28c2cce0b58dd6f29b6b4dbf0f3c6e28fee202a
Coty Beasley <coty@cotybeasley.name>
2018-12-23 12:21:34 -0600

    Added mapbox iframe

--- src/layouts/Main.vue
+++ src/layouts/Main.vue
@@ -6,6 +6,9 @@
         <slot></slot>
       </main>
     </div>
+    <div id="map">
+      <iframe src="https://api.mapbox.com/styles/v1/wcfp/cjq10iet01waa2rqjv4f6uuuu.html?fresh=true&title=true&access_token=pk.eyJ1Ijoid2NmcCIsImEiOiJjanExMGd1aDYwc2w3NDJyMXlhNGpwd3NsIn0.kzJ7I0HYbEkzhKAN4cmB-Q#14.1/39.181795/-96.669735/0" frameborder="0" height="100%" width="100%"></iframe>
+    </div>
   </div>
 </template>
 
@@ -28,12 +31,6 @@
     height: 100%;
   }
 
-  main {
-    flex-direction: column;
-    overflow-x: hidden;
-    overflow-y: scroll;
-  }
-
   #contentBody {
     background: white;
     height: 100%;
@@ -42,6 +39,7 @@
     transition: all 0.3s ease-out;
     display: flex;
     flex-flow: column;
+    flex: 1 auto;
   }
 
   @media screen and (min-width: 70rem) {
@@ -51,7 +49,14 @@
     }
   }
 
+  #map {
+    flex: 1 100%;
+  }
+
   main {
+    flex-direction: column;
+    overflow-x: hidden;
+    overflow-y: scroll;
     padding: $padding_side;
     background: #f9f7f5;
   }
