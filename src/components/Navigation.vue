724992bb3820ef0ef5df0a9f981e9551a500aad8
Coty Beasley <coty@cotybeasley.name>
2018-12-23 11:36:51 -0600

    Reset project to the basics

---
+++ src/components/Navigation.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="navigation">
+    <nav>
+      <v-link href="/">Home</v-link>
+      <v-link href="/about">About</v-link>
+    </nav>
+  </div>
+</template>
+
+
+<script>
+  import VLink from '../components/VLink.vue'
+
+  export default {
+    components: {
+      VLink
+    },
+    data() {
+      return {
+        msg: 'This is a message'
+      }
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+  // Imports
+  @import "../globalStyles/global";
+
+  #navigation {
+    background: black;
+    color: white;
+    padding: 24px;
+  }
+  a {
+    margin: 0 24px;
+    text-decoration: none;
+    &:active, &:visited {
+      color: orange;
+    }
+  }
+</style>
---
+++ src/layouts/Main.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <Nav></Nav>
+    <main>
+      <slot></slot>
+    </main>
+  </div>
+</template>
+
+<script>
+  import Nav from '../components/Navigation.vue'
+
+  export default {
+    components: {
+      Nav
+    }
+  }
+</script>
+
+<style lang="scss">
+  // Imports
+  @import "../globalStyles/global";
+
+  #app {
+    display: flex;
+    flex-direction: column;
+  }
+
+  main {
+    padding: $padding_side;
+    background: #f9f7f5;
+  }
+</style>
